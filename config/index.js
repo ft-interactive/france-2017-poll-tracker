2594fbb7216b162900bd07e5e0273dcbe9583bc6
Callum Locke <callum.locke@gmail.com>
2017-02-27 18:03:33 +0000

    use single spreadsheet; use candidate keys defined in spreadsheet

--- config/index.js
+++ config/index.js
@@ -7,50 +7,14 @@ export default async () => {
   const d = await article();
   const flags = await getFlags();
   const onwardJourney = await getOnwardJourney();
-  const endpointProfiles = `https://bertha.ig.ft.com/${process.env.REPUBLISH ? 'republish' : 'view'}/publish/gss/${process.env.SPREADSHEET_PROFILES}/data`;
-  const endpointPolls = `https://bertha.ig.ft.com/${process.env.REPUBLISH ? 'republish' : 'view'}/publish/gss/${process.env.SPREADSHEET_POLLS}/data,data_round2,microcopy`;
-
   const combinedEndpoint = `https://bertha.ig.ft.com/${process.env.REPUBLISH ? 'republish' : 'view'}/publish/gss/${process.env.SPREADSHEET_KEY}/polls,candidates`;
-  /*
-  An experimental demo that gets content from the API
-  and overwrites some model values. This requires the Link File
-  to have been published. Also next-es-interface.ft.com probably
-  isn't a reliable source. Also this has no way to prevent development
-  values being seen in productions... use with care.
 
-  try {
-    const a = (await axios(`https://next-es-interface.ft.com/content/${d.id}`)).data;
-    d.headline = a.title;
-    d.byline = a.byline;
-    d.summary = a.summaries[0];
-    d.title = d.title || a.title;
-    d.description = d.description || a.summaries[1] || a.summaries[0];
-    d.publishedDate = new Date(a.publishedDate);
-    f.comments = a.comments;
-  } catch (e) {
-    console.log('Error getting content from content API');
-  }
-
-  */
-
-  const combined = (await axios.get(combinedEndpoint)).data;
-  const profiles = (await axios.get(endpointProfiles)).data;
-  const polls = (await axios.get(endpointPolls)).data;
-  profiles.pop(); // Remove Bayrou for now
-
-
-  const data = {
-    candidates: profiles,
-    polls,
-    microcopy: polls.microcopy,
-  };
+  const data = (await axios.get(combinedEndpoint)).data;
 
   return {
     ...d,
     flags,
     onwardJourney,
     data,
-
-    combinedData: combined,
   };
 };
