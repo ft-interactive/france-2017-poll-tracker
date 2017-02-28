7369dabeaac808b665b0ead7d2047b33afe186cd
Callum Locke <callum.locke@gmail.com>
2017-02-28 14:10:28 +0000

    refactor chart

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ export default async () => {
   const d = await article();
   const flags = await getFlags();
   const onwardJourney = await getOnwardJourney();
-  const combinedEndpoint = `https://bertha.ig.ft.com/${process.env.REPUBLISH ? 'republish' : 'view'}/publish/gss/${process.env.SPREADSHEET_KEY}/polls,candidates`;
+  const combinedEndpoint = `https://bertha.ig.ft.com/${process.env.REPUBLISH ? 'republish' : 'view'}/publish/gss/${process.env.SPREADSHEET_KEY}/candidates,round1,round2`;
 
   const data = (await axios.get(combinedEndpoint)).data;
 
@@ -16,5 +16,36 @@ export default async () => {
     flags,
     onwardJourney,
     data,
+
+    charts: {
+      round1: {
+        lines: data.candidates.map(({ color, name, key }) => ({
+          color,
+          label: name.last,
+          points: data.round1.reduce((result, poll) => {
+            // TODO adjust value with weighting logic
+
+            const value = poll.result[key];
+
+            if (value) {
+              result.push({
+                date: poll.date,
+                value,
+              });
+            }
+
+            return result;
+          }, []),
+        })),
+        minValue: 0,
+        maxValue: 60,
+      },
+
+      round2: {
+        // TODO
+        scenario1: {},
+        scenario2: {},
+      },
+    },
   };
 };
